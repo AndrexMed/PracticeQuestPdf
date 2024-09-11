141efa51894ff0f165679c70f439fb5c5f3c9f76
GiovanniAndrex <giovanniandrex@gmail.com>
2024-09-10 23:24:47 -0500

    Creacion de table

--- ReportePDF/Program.cs
+++ ReportePDF/Program.cs
@@ -9,52 +9,114 @@ Document.Create(document =>
 {
     document.Page(page =>
     {
-        //page.Size(PageSizes.A4);
-        page.Margin(2, Unit.Centimetre);
+        page.Size(PageSizes.A4);
+        page.Margin(1, Unit.Centimetre);
         page.PageColor(Colors.White);
         //page.DefaultTextStyle(x => x.FontSize(20));
 
-        //page.Header()
-        //    .Text("Hello PDF!")
-        //    .SemiBold().FontSize(36).FontColor(Colors.Blue.Medium);
-        page.Header().Row(row =>
+        page.Header().ShowOnce().Row(row =>
         {
             row.ConstantItem(140).Height(60).Placeholder(); //Logo
 
             row.RelativeItem().Column(col =>
             {
-                col.Item().AlignCenter().Text("Giovanny Alonso Alzate S.A.S").Bold().FontSize(14);
-                col.Item().AlignCenter().Text("Bello Antioquia").FontSize(9);
-                col.Item().AlignCenter().Text("321 883 3424").FontSize(9);
-                col.Item().AlignCenter().Text("giovanny-0001@hotmail.com").FontSize(9);
+                col.Item().AlignCenter().Text("Giovanny Alonso Alzate S.A").Bold().FontSize(14);
+                col.Item().AlignCenter().Text("Bello Antioquia").FontSize(11);
+                col.Item().AlignCenter().Text("123 456 78 99").FontSize(11);
+                col.Item().AlignCenter().Text("giovanny@hotmail.com").FontSize(11);
                 //col.Item().Background(Colors.Orange.Medium).Height(10);
             });
 
             row.RelativeItem().Border(1).Column(col =>
             {
-                col.Item().Border(1).BorderColor("#257272").AlignCenter().Text($"Fecha: {DateTime.Now.ToShortDateString()}").Bold().FontSize(14);
-                col.Item().Background("#257272").AlignCenter().Text($"Cotizacion Nº: {Math.Floor(5.0)}").FontSize(9);
-                col.Item().AlignCenter().Text("987 987 123/ 02020202").FontSize(9);
-                col.Item().AlignCenter().Text("giovanny-0001@hotmail.com").FontSize(9);
+                col.Item().Background("#257272").AlignCenter().Text($"Cotizacion Nº: {Math.Floor(5.0)}").FontSize(14).SemiBold();
+                col.Item().Border(1).BorderColor("#257272").AlignCenter().Text($"Fecha: {DateTime.Now.ToShortDateString()}").Bold().FontSize(12);
+                col.Item().AlignCenter().Text("Medellin - Antioquia").FontSize(9);
             });
         });
 
-        page.Content()
-            .PaddingVertical(1, Unit.Centimetre)
-            .Column(x =>
+        page.Content().PaddingVertical(10).Column(col1 =>
+        {
+            col1.Item().Column(col2 =>
             {
-                x.Spacing(20);
+                col2.Item().Text("Datos del Cliente").Underline().Bold().FontSize(12);
 
-                x.Item().Text(Placeholders.LoremIpsum());
-                x.Item().Image(Placeholders.Image(200, 100));
+                col2.Item().Text(text =>
+                {
+                    text.Span("Nombre/Empresa: ").SemiBold().FontSize(11);
+                    text.Span("Capricentro").FontSize(11);
+                });
+
+                col2.Item().Text(text =>
+                {
+                    text.Span("Direccion ").SemiBold().FontSize(11);
+                    text.Span("Itagui Antioquia").FontSize(11);
+                });
+
+                col2.Item().Text(text =>
+                {
+                    text.Span("Contacto: ").SemiBold().FontSize(11);
+                    text.Span("12334566").FontSize(11);
+                });
             });
 
+            col1.Item().PaddingVertical(5).LineHorizontal(0.5f);
+
+            col1.Item().Table(table =>
+            {
+                table.ColumnsDefinition(cols =>
+                {
+                    cols.RelativeColumn(3);
+                    cols.RelativeColumn();
+                    cols.RelativeColumn();
+                    cols.RelativeColumn();
+                });
+
+                table.Header(header =>
+                {
+                    header.Cell().Background("257272").Padding(2).Text("Descripcion").FontColor(Colors.White);
+                    header.Cell().Background("257272").Padding(2).Text("Precio Unitario").FontColor(Colors.White);
+                    header.Cell().Background("257272").Padding(2).Text("Cantidad").FontColor(Colors.White);
+                    header.Cell().Background("257272").Padding(2).Text("Total").FontColor(Colors.White);
+                });
+
+                foreach (var item in Enumerable.Range(1, 45))
+                {
+                    var cantidad = Placeholders.Random.Next(1, 10);
+                    var precio = Placeholders.Random.Next(5, 15);
+                    var total = cantidad * precio;
+
+                    table.Cell().BorderBottom(0.5f).BorderColor("#D9D9D9").Padding(2).Text(Placeholders.Label()).FontSize(10);
+
+                    table.Cell().BorderBottom(0.5f).BorderColor("#D9D9D9").Padding(2).Text($"{precio.ToString()}").FontSize(10);
+
+                    table.Cell().BorderBottom(0.5f).BorderColor("#D9D9D9").Padding(2).Text(cantidad.ToString()).FontSize(10);
+
+                    table.Cell().BorderBottom(0.5f).BorderColor("#D9D9D9").Padding(2).Text(total.ToString()).FontSize(10);
+                }
+
+                col1.Item().PaddingRight(1, Unit.Centimetre).PaddingVertical(5).AlignRight().Text("Total: $1500").Bold().FontSize(12);
+
+                col1.Item().Background(Colors.Grey.Lighten3).Padding(10).Column(col =>
+                {
+                    col.Item().Text("Comentarios").FontSize(14);
+                    col.Item().Text(Placeholders.LoremIpsum());
+                    col.Spacing(5);
+                });
+
+                col1.Spacing(0);
+            });
+
+        });
+
         page.Footer()
-            .AlignCenter()
+            .AlignRight()
             .Text(x =>
             {
-                x.Span("Page ");
+                x.Span("Pagina ");
                 x.CurrentPageNumber();
+                x.Span(" de ");
+                x.TotalPages();
             });
     });
 })
